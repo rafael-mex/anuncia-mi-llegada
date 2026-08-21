735a94f944b12cf7c24b1d73d0cfa441d9f3a84e
rafael-mex <rafael_mex@comunidad.unam.mx>
2026-08-20 21:42:08 -0600

    feat(Orientation): Se bloquea la orientación a solo vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'config/router/app_router.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const AnunciaMiLlegadaApp());
 }
 
