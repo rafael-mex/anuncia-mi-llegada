1882e82097a7cde2cc343442421ccfb9de05b7ef
rafael-mex <rafael_mex@comunidad.unam.mx>
2026-08-21 13:27:33 -0600

    fix(shared): resuelta la excepción del ParentDataWidget con el ReturnButton

--- lib/presentation/widgets/shared/return_button.dart
+++ lib/presentation/widgets/shared/return_button.dart
@@ -12,50 +12,45 @@ class ReturnButton extends StatelessWidget {
       right: 0,
       top: 670,
       child: Center(
-        child: Positioned(
-          left: 0,
-          right: 0,
-          top: 706,
-          child: Opacity(
-            opacity: 0.87,
-            child: InnerShadow(
-              shadows: const [
-                Shadow(
-                  color: Color.fromRGBO(255, 255, 255, 40),
-                  blurRadius: 2,                  offset: Offset(1, 1),
-                ),
-              ],
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: Material(
-                  color: Color.fromRGBO(113, 203, 248, 100),
-                  child: InkWell(
-                    highlightColor: Colors.transparent,
-                    splashColor: Colors.transparent,
-                    onTap: context.pop,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 10,
-                      ),
-                      child: InnerShadow(
-                        shadows: const [
-                            Shadow(
-                              color: Color.fromRGBO(255, 255, 255, 44),
-                              blurRadius: 10,
-                              offset: Offset(1, 1),
-                            ),
-                          ],
-                        child: const Text(
-                          'Retroceder',
-                          style: TextStyle(
-                            color: Color.fromRGBO(255, 255, 255, 83),
-                            height: 1.3,
-                            fontFamily: 'Nunito',
-                            fontSize: 17,
-                            letterSpacing: 0,
-                            fontWeight: FontWeight.w700,
+        child: Opacity(
+          opacity: 0.87,
+          child: InnerShadow(
+            shadows: const [
+              Shadow(
+                color: Color.fromRGBO(255, 255, 255, 40),
+                blurRadius: 2,                  offset: Offset(1, 1),
+              ),
+            ],
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Material(
+                color: Color.fromRGBO(113, 203, 248, 100),
+                child: InkWell(
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  onTap: context.pop,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
+                    ),
+                    child: InnerShadow(
+                      shadows: const [
+                          Shadow(
+                            color: Color.fromRGBO(255, 255, 255, 44),
+                            blurRadius: 10,
+                            offset: Offset(1, 1),
                           ),
+                        ],
+                      child: const Text(
+                        'Retroceder',
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 83),
+                          height: 1.3,
+                          fontFamily: 'Nunito',
+                          fontSize: 17,
+                          letterSpacing: 0,
+                          fontWeight: FontWeight.w700,
                         ),
                       ),
                     ),
