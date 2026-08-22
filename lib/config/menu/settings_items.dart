eb6b3119c403cb1e2ea83453640c8b761abbc620
rafael-mex <rafael_mex@comunidad.unam.mx>
2026-08-21 20:54:26 -0600

    feat(settings): se agrega la tipografía de METRO-DF, el title y subtitle de las opciones, su icono personalizado y la animación de cambio del light modea dark mode

---
+++ lib/config/menu/settings_items.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:light_dark_theme_toggle/light_dark_theme_toggle.dart';
+
+class MenuItem {
+  final Widget title;
+  final Widget subtitle;
+  final Widget icon;
+
+  MenuItem({required this.title, required this.subtitle, required this.icon});
+}
+
+final appSettingsItems = <MenuItem>[
+  //Option: Estaciones
+  MenuItem(
+    title: Text(
+      "Estaciones",
+      style: TextStyle(fontFamily: 'METRO-DF', fontSize: 24),
+    ),
+    subtitle: Text(
+      "Configura el como aparecen las \nestaciones en la aplicación.",
+      style: TextStyle(
+        color: Color.fromRGBO(91, 79, 79, 100),
+        height: 0,
+        fontFamily: 'Nunito',
+        fontSize: 12,
+        letterSpacing: 0,
+        fontWeight: FontWeight.w700,
+      ),
+    ),
+    icon: Image.asset(
+      'assets/icons/config_icons/estaciones.png',
+      fit: BoxFit.contain,
+    ),
+  ),
+
+  //Option: Mensajes
+  MenuItem(
+    title: Text(
+      "Mensajes",
+      style: TextStyle(fontFamily: 'METRO-DF', fontSize: 24),
+    ),
+    subtitle: Text(
+      "Personaliza el mensaje que \nenviarás a tus contactos.",
+      style: TextStyle(
+        color: Color.fromRGBO(91, 79, 79, 100),
+        height: 1,
+        fontFamily: 'Nunito',
+        fontSize: 12,
+        letterSpacing: 0,
+        fontWeight: FontWeight.w700,
+      ),
+    ),
+    icon: Image.asset(
+      'assets/icons/config_icons/mensajes.png',
+      fit: BoxFit.contain,
+    ),
+  ),
+
+  //Option: Apariencia
+  MenuItem(
+    title: Text(
+      "Apariencia",
+      style: TextStyle(fontFamily: 'METRO-DF', fontSize: 24),
+    ),
+    subtitle: Text(
+      "Cambia entre el modo claro y \nobscuro.",
+      style: TextStyle(
+        color: Color.fromRGBO(91, 79, 79, 100),
+        height: 0,
+        fontFamily: 'Nunito',
+        fontSize: 12,
+        letterSpacing: 0,
+        fontWeight: FontWeight.w700,
+      ),
+    ),
+    icon: const AppearanceIcon(),
+  ),
+];
+
+final appearanceMode = ValueNotifier<bool>(true);
+
+class AppearanceIcon extends StatelessWidget {
+  const AppearanceIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+      //Imagen
+        Image.asset(
+          'assets/icons/config_icons/apariencia.png',
+          width: 100,
+          height: 100,
+          fit: BoxFit.contain,
+        ),
+      //Animación
+        ValueListenableBuilder<bool>(
+          valueListenable: appearanceMode,
+          builder: (context, isLightMode, _) => LightDarkThemeToggle(
+            value: isLightMode,
+            onChanged: (value) => appearanceMode.value = value,
+            themeIconType: ThemeIconType.expand,
+            color: Colors.white,
+            size: 41,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/presentation/screens/settings/settings_screen.dart
+++ lib/presentation/screens/settings/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:anuncia_mi_llegada/config/menu/settings_items.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -16,7 +17,7 @@ class SettingsScreen extends StatelessWidget {
           Positioned(
             left: 0,
             right: 0,
-            top: 115,
+            top: 100,
             child: Center(
               child: Image(
                 image: AssetImage('assets/icons/config_icons/gear_white.png'),
@@ -25,22 +26,94 @@ class SettingsScreen extends StatelessWidget {
               ),
             ),
           ),
-          //Back button
+          //Keyboard Return button
           Positioned(
             left: 19,
-            top: 150,
+            top: 128,
             child: IconButton(
               onPressed: () {
                 context.pop();
               },
               style: ButtonStyle(
                 iconSize: WidgetStatePropertyAll(24),
-                iconColor: WidgetStatePropertyAll(Color.fromRGBO(224, 114, 45, 100)),
+                iconColor: WidgetStatePropertyAll(
+                  Color.fromRGBO(224, 114, 45, 100),
+                ),
               ),
               icon: Icon(Icons.keyboard_return_outlined),
             ),
           ),
+          //Options
+          _SettingsView(),
         ],
+      ),
+    );
+  }
+}
+
+class _SettingsView extends StatelessWidget {
+  const _SettingsView();
+
+  @override
+  Widget build(BuildContext context) {
+    appSettingsItems;
+
+    return Positioned.fill(
+      child: Center(
+        child: ListView.builder(
+          shrinkWrap: true,
+          padding: EdgeInsets.zero,
+          itemCount: appSettingsItems.length,
+          itemBuilder: (BuildContext context, int index) {
+            final menuItem = appSettingsItems[index];
+
+            return _CustomListTitle(menuItem: menuItem);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+  const _CustomListTitle({required this.menuItem});
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return InkWell(
+      onTap: () {
+        if (menuItem.icon is AppearanceIcon) {
+          appearanceMode.value = !appearanceMode.value;
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+        child: Row(
+          children: [
+            SizedBox(
+              width: 70,
+              height: 100,
+              child: menuItem.icon,
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  menuItem.title,
+                  menuItem.subtitle,
+                ],
+              ),
+            ),
+            if (menuItem.icon is! AppearanceIcon)
+              Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+          ],
+        ),
       ),
     );
   }
