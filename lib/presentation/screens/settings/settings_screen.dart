47e3d6d2c99306d645751f866c8bdbdabaad82ee
rafael-mex <rafael_mex@comunidad.unam.mx>
2026-08-23 00:58:46 -0600

    feat(settings): engranaje dinámico con cross-fade y textos que cambian de color según el modo

--- lib/presentation/screens/settings/settings_screen.dart
+++ lib/presentation/screens/settings/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:anuncia_mi_llegada/config/menu/settings_items.dart';
+import 'package:anuncia_mi_llegada/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,43 +10,66 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          //Gear Icon for Light Mode
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 100,
-            child: Center(
-              child: Image(
-                image: AssetImage('assets/icons/config_icons/gear_white.png'),
-                width: 104,
-                height: 104,
-              ),
-            ),
+      backgroundColor: Colors.transparent,
+      body: ValueListenableBuilder<bool>(
+        valueListenable: isTrueDarkMode,
+        builder: (context, isDark, _) => AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+          decoration: BoxDecoration(
+            color: isDark ? null : AppTheme.backgroundColorLM,
+            gradient: isDark ? AppTheme.backgroundColorDM : null,
           ),
-          //Keyboard Return button
-          Positioned(
-            left: 19,
-            top: 128,
-            child: IconButton(
-              onPressed: () {
-                context.pop();
-              },
-              style: ButtonStyle(
-                iconSize: WidgetStatePropertyAll(24),
-                iconColor: WidgetStatePropertyAll(
-                  Color.fromRGBO(224, 114, 45, 100),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                right: 0,
+                top: 100,
+                child: Center(
+                  child: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 500),
+                    child: Image(
+                      key: ValueKey<String>(
+                        isDark
+                            ? 'assets/icons/config_icons/gear_dark.png'
+                            : 'assets/icons/config_icons/gear_white.png',
+                      ),
+                      image: AssetImage(
+                        isDark
+                            ? 'assets/icons/config_icons/gear_dark.png'
+                            : 'assets/icons/config_icons/gear_white.png',
+                      ),
+                      width: 104,
+                      height: 104,
+                    ),
+                  ),
                 ),
               ),
-              icon: Icon(Icons.keyboard_return_outlined),
-            ),
+              //Keyboard Return button
+              Positioned(
+                left: 19,
+                top: 128,
+                child: IconButton(
+                  onPressed: () {
+                    context.pop();
+                  },
+                  style: ButtonStyle(
+                    iconSize: WidgetStatePropertyAll(24),
+                    iconColor: WidgetStatePropertyAll(
+                      Color.fromRGBO(224, 114, 45, 100),
+                    ),
+                  ),
+                  icon: Icon(Icons.keyboard_return_outlined),
+                ),
+              ),
+              //Options
+              _SettingsView(),
+            ],
           ),
-          //Options
-          _SettingsView(),
-        ],
+        ),
       ),
     );
   }
@@ -79,42 +103,69 @@ class _CustomListTitle extends StatelessWidget {
   const _CustomListTitle({required this.menuItem});
 
   final MenuItem menuItem;
+  Widget _withColor(Widget widget, Color color) {
+    if (widget is Text && widget.data != null) {
+      final baseStyle = widget.style ?? const TextStyle();
+      return TweenAnimationBuilder<Color?>(
+        tween: ColorTween(begin: color, end: color),
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+        builder: (context, animatedColor, _) => Text(
+          widget.data!,
+          style: baseStyle.copyWith(color: animatedColor ?? color),
+        ),
+      );
+    }
+    return widget;
+  }
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
 
-    return InkWell(
-      onTap: () {
-        if (menuItem.icon is AppearanceIcon) {
-          appearanceMode.value = !appearanceMode.value;
-        }
+    return ValueListenableBuilder<bool>(
+      valueListenable: isTrueDarkMode,
+      builder: (context, isDark, _) {
+
+        final titleColor = isDark
+            ? const Color.fromRGBO(204, 204, 204, 100)
+            : Colors.black;
+        final subtitleColor = isDark
+            ? const Color.fromRGBO(151, 145, 145, 100)
+            : const Color.fromRGBO(91, 79, 79, 100);
+
+        return InkWell(
+          onTap: () {
+            if (menuItem.icon is AppearanceIcon) {
+              isTrueDarkMode.value = !isTrueDarkMode.value;
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 12.0,
+            ),
+            child: Row(
+              children: [
+                SizedBox(width: 70, height: 100, child: menuItem.icon),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      _withColor(menuItem.title, titleColor),
+                      _withColor(menuItem.subtitle, subtitleColor),
+                    ],
+                  ),
+                ),
+                if (menuItem.icon is! AppearanceIcon)
+                  Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+              ],
+            ),
+          ),
+        );
       },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-        child: Row(
-          children: [
-            SizedBox(
-              width: 70,
-              height: 100,
-              child: menuItem.icon,
-            ),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  menuItem.title,
-                  menuItem.subtitle,
-                ],
-              ),
-            ),
-            if (menuItem.icon is! AppearanceIcon)
-              Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
-          ],
-        ),
-      ),
     );
   }
 }
